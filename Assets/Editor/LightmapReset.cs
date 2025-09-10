4dea070e91f2e5f4b71f72c83812abd15a18b67b
Insectscorch <mujica@hey.com>
2025-09-10 05:19:35 -0500

    Iluminacion corregida de la casa. Menu casa con escenario y logo Plastigama. Nuevos elementos UI. Fuente Bungee Spice

---
+++ Assets/Editor/LightmapReset.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class LightmapReset : MonoBehaviour
+{
+    [MenuItem("Tools/Clear Lightmap Data From Scene")]
+    static void ClearLightmapData()
+    {
+        Renderer[] renderers = FindObjectsOfType<Renderer>();
+
+        foreach (Renderer r in renderers)
+        {
+            // Forzar lightmap index a -1
+            SerializedObject so = new SerializedObject(r);
+            var prop = so.FindProperty("m_LightmapIndex");
+            if (prop != null)
+            {
+                prop.intValue = -1;
+                so.ApplyModifiedProperties();
+            }
+
+            // Desactivar contribución a GI si está presente
+            GameObjectUtility.SetStaticEditorFlags(r.gameObject,
+                GameObjectUtility.GetStaticEditorFlags(r.gameObject) & ~StaticEditorFlags.ContributeGI);
+        }
+
+        // Borra datos bakeados de la escena
+        Lightmapping.Clear();
+
+        Debug.Log("✅ Todos los objetos quedaron libres de lightmaps y GI.");
+    }
+}
